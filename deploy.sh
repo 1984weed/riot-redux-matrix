ddc757589b680b537fc45f64e7b4fd02300994d1
Teruo Kunihiro <yuyuyu1999@gmail.com>
2016-12-09 00:58:44 +0900

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -22,9 +22,12 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
 mkdir dist
+
+git clone $REPO dist
+
 cd dist
+
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
